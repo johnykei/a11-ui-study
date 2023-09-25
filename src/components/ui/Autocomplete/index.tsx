bd6a7ff76340ed094aef1b628ea463cb6c7a1145
Johnny Okafuji <okafuji@nulab.com>
2023-09-25 21:16:27 +0900

    :+1: add state to toggle the menu

--- src/components/ui/Autocomplete/index.tsx
+++ src/components/ui/Autocomplete/index.tsx
@@ -5,16 +5,25 @@ interface AutocompleteProps {
 }
 
 const Autocomplete: React.FC<AutocompleteProps> = ({ title }) => {
+  const [isShowMenu, setShowMenu] = React.useState(false);
   return (
     <>
-      <input type="search" role="combobox" aria-autocomplete="list" />
-      <div>
-        <h2>{title}</h2>
-        <ul>
-          <li>aaa</li>
-          <li>aaa</li>
-        </ul>
-      </div>
+      <input
+        type="search"
+        role="combobox"
+        onFocus={() => setShowMenu(true)}
+        onBlur={() => setShowMenu(false)}
+        aria-autocomplete="list"
+      />
+      {isShowMenu && (
+        <div>
+          <h2 id="autocomplete_title">{title}</h2>
+          <ul aria-labelledby="autocomplete_title" role="listbox">
+            <li role="option">aaa</li>
+            <li role="option">aaa</li>
+          </ul>
+        </div>
+      )}
     </>
   );
 };
