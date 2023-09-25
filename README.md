5a97a37eaa7bb9ad5add1315b027ac40aefd6dff
Johnny Okafuji <okafuji@nulab.com>
2023-09-25 20:58:01 +0900

    :zap: create an enviornment with React + TypeScript + Vite.js

---
+++ README.md
@@ -0,0 +1,34 @@
+# Accessibility UI Study
+
+This repository is for personal space to study a11y UI.
+
+## Documentations
+
+This repository uses the following tools.
+
+- [React](https://react.dev/)
+- [TypeScript](https://www.typescriptlang.org/)
+- [Vite.js](https://vitejs.dev/)
+- [Tailwind CSS](https://tailwindcss.com/)
+- [Storybook](https://storybook.js.org/)
+
+## Prerequisites
+
+- [Node.js](http://nodejs.org/) (v18.18.0)
+
+Fix the version of Node.js to reduce the risk of version differences changing the operating environment. (Current: **v18.18.0**)
+Recommend to use [asdf](https://asdf-vm.com/) or something like that which provide multiple node environments. `.tool-versions` file in the root of the project fixes the Node.js version.
+
+## Getting Started
+
+### Develop
+
+```
+npm run dev
+```
+
+### Storybook
+
+```
+npm run storybook
+```
---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import ModalWithDialogEl from "./components/ui/ModalWithDialogEl";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <ModalWithDialogEl title="Hello">
+        Lorem ipsum dolor sit amet,{" "}
+        <a href="https://johnykei.net/">consectetuer adipiscing elit</a>. Aenean
+        commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et
+        magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
+        quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
+        arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+        Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras
+        dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend
+        tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
+        enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+        Phasellus viverra nulla ut metus varius laoreet.
+      </ModalWithDialogEl>
+
+      <a href="https://johnykei.net/">test link</a>
+    </>
+  );
+}
+
+export default App;
