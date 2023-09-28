3c4f6c394c9a8976bb0423b4a371caeb23dd5e43
Johnny Okafuji <okafuji@nulab.com>
2023-09-28 18:39:27 +0900

    :+1: enable to control autocomplete with keyboard

--- src/components/ui/Autocomplete/index.tsx
+++ src/components/ui/Autocomplete/index.tsx
@@ -17,14 +17,60 @@ const Autocomplete: React.FC<AutocompleteProps> = ({
   autocomplete,
 }) => {
   const [isShowAutocomplete, setShowAutocomplete] = React.useState(false);
+  const [activeIndex, setActiveIndex] = React.useState(-1);
+
+  const handleBlur = () => {
+    setShowAutocomplete(false);
+    setActiveIndex(-1);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    switch (e.key) {
+      case "Enter":
+        if (activeIndex >= 0) {
+          const selectedElement = document.getElementById(
+            `${autocompleteId}_item_${activeIndex}`
+          );
+          if (selectedElement) {
+            selectedElement.click();
+          }
+        }
+
+        break;
+      case "ArrowDown":
+        autocomplete && activeIndex !== autocomplete.length - 1
+          ? setActiveIndex((prevIndex) =>
+              Math.min(prevIndex + 1, autocomplete.length - 1)
+            )
+          : setActiveIndex(0);
+        break;
+      case "ArrowUp":
+        if (autocomplete) {
+          e.preventDefault();
+          setActiveIndex((prevIndex) =>
+            prevIndex === 0
+              ? autocomplete.length - 1
+              : Math.max(prevIndex - 1, 0)
+          );
+        }
+        break;
+      case "Escape":
+        e.preventDefault();
+        setActiveIndex(-1);
+        break;
+    }
+  };
+
   return (
     <>
       <input
         type="search"
         role="combobox"
         placeholder="Search..."
+        id={`${autocompleteId}_input`}
         onFocus={() => setShowAutocomplete(true)}
-        onBlur={() => setShowAutocomplete(false)}
+        onBlur={handleBlur}
+        onKeyDown={handleKeyDown}
         autoComplete="off"
         aria-controls={`${autocompleteId}_list`}
         aria-autocomplete="list"
@@ -40,8 +86,15 @@ const Autocomplete: React.FC<AutocompleteProps> = ({
               role="listbox"
             >
               {autocomplete.map((item, i) => (
-                <li role="option" key={`autocomplete-${i}`}>
-                  <a href={item.href}>{item.title}</a>
+                <li
+                  role="option"
+                  key={`autocomplete-${i}`}
+                  aria-selected={i === activeIndex}
+                  className="aria-selected:bg-gray-800"
+                >
+                  <a href={item.href} id={`${autocompleteId}_item_${i}`}>
+                    {item.title}
+                  </a>
                 </li>
               ))}
             </ul>
